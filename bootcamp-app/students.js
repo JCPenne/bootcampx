db995804ac179928c7e8311b756ed804d1cc7b9c
JCPenne <jackcpalmer@icloud.com>
2022-08-01 17:32:46 -0700

    parameterize queries in students.js and teachers.js

--- bootcamp-app/students.js
+++ bootcamp-app/students.js
@@ -1,6 +1,9 @@
 const { Pool } = require('pg');
 const process = require('process');
 const arguments = process.argv.slice(2);
+const cohortName = arguments[0];
+const limit = arguments[1] || 5;
+const values = [`%${cohortName}%`, limit];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -9,16 +12,16 @@ const pool = new Pool({
   database: 'bootcampx',
 });
 
-pool
-  .query(
-    `
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${arguments[0]}%'
-LIMIT ${arguments[1] || 5};
-`,
-  )
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+pool
+  .query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp-app/teachers.js
+++ bootcamp-app/teachers.js
@@ -1,6 +1,19 @@
 const { Pool } = require('pg');
 const process = require('process');
 const arguments = process.argv.slice(2);
+const cohortName = arguments[0];
+const values = [`%${cohortName}%`];
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+    FROM assistance_requests
+    JOIN teachers on teachers.id = teacher_id
+    JOIN students on students.id = student_id
+    JOIN cohorts on cohorts.id = cohort_id
+    WHERE cohorts.name LIKE $1
+    GROUP BY teachers.name, cohorts.name
+    ORDER BY teacher
+`;
 
 const pool = new Pool({
   user: 'vagrant',
@@ -10,17 +23,7 @@ const pool = new Pool({
 });
 
 pool
-  .query(
-    `
-    SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-    FROM assistance_requests
-    JOIN teachers on teachers.id = teacher_id
-    JOIN students on students.id = student_id
-    JOIN cohorts on cohorts.id = cohort_id
-    WHERE cohorts.name LIKE '${arguments[0]}'
-    GROUP BY teachers.name, cohorts.name
-    ORDER BY teacher`,
-  )
+  .query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher}`);
